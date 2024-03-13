1e094199ae1806ad4420b2add6c847353f806cd3
Aman <amanparasher.ai@gmail.com>
2024-03-13 23:15:36 +0530

    changes

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ import numpy as np
 app=Flask(__name__)
 camera = cv2.VideoCapture(0)
 # Load a sample picture and learn how to recognize it.
-krish_image = face_recognition.load_image_file("Krish/krish.jpg")
+krish_image = face_recognition.load_image_file("Aman.jpg")
 krish_face_encoding = face_recognition.face_encodings(krish_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-bradley_image = face_recognition.load_image_file("Bradley/bradley.jpg")
+bradley_image = face_recognition.load_image_file("bradley.jpg")
 bradley_face_encoding = face_recognition.face_encodings(bradley_image)[0]
 
 # Create arrays of known face encodings and their names
@@ -20,7 +20,7 @@ known_face_encodings = [
     bradley_face_encoding
 ]
 known_face_names = [
-    "Krish",
+    "Aman",
     "Bradly"
 ]
 # Initialize some variables
